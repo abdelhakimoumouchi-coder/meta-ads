ae0ce24d748501a40f5d419a980576d89e5ab652
abdelhakim <abdelhakim.oumouchi@edu.ensa.dz>
2026-03-27 00:09:59 +0100

    fix postcss config syntax

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,6 @@
-/** @type {import('postcss-load-config').Config} */
 module.exports = {
-const config = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
   },
 };
-};
-
-module.exports = config;
